bfd43ac56f32fecf023e6ccd64fc29034ab189a9
mo7amad7amdy <mohammad-hamdy@hotmail.com>
2021-07-29 10:22:52 +0200

    fix bug and change login page design and handel news api

---
+++ app/Http/Controllers/API/V1/NewsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\NewsResource;
+use App\Models\Language;
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index(News $news, Request $request){
+        $perPage = $request->get('per_page', 15);
+        $lang = Language::find($request->lang);
+        if (!isset($lang)){
+            return response()->json([
+                'status' => 404,
+                'message' => 'This Language not found!',
+            ], '404');
+        }
+        return NewsResource::collection($news->where('language_id', $lang->id)->orderBy('id', 'desc')->paginate($perPage)
+            ->appends([
+                'per_page' => $perPage
+            ])
+        );
+    }
+    public function show(News $news, Request $request){
+        return NewsResource::collection($news->where('id', $request->id)->get());
+    }
+}
---
+++ app/Http/Resources/NewsResource.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Http\Resources;
+
+use \Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class NewsResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        if (Storage::exists($this->image)){
+            $this['image'] = asset(Storage::url($this->image));
+        }else {
+            $this['image'] = asset('media/default_image.png');
+        }
+
+        return $data = [
+            'id' => $this->id,
+            'title' => $this->title,
+            'caption' => $this->caption,
+            'image' => asset($this->image),
+            'content' => $this->content,
+            'created_at' => (string)$this->created_at,
+            'updated_at' => (string)$this->updated_at
+        ];
+
+    }
+}
